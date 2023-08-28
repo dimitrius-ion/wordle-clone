50a9972dca3e567dc166244a2386b92f1e7b714e
Dimitrius Ion <dimi@ion.lan>
2023-08-28 12:01:46 -0700

    adding Game logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,8 @@ import Input from "../Input";
 import Guess from "../Guess";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 let guesses = range(0, NUM_OF_GUESSES_ALLOWED);
@@ -20,7 +22,13 @@ function Game() {
       <div className="guess-results">
         {guesses.map((guess) => (
           <div key={guess} className="guess-results">
-            <Guess guess={wordList[guess]} />
+            <Guess
+              guess={
+                wordList[guess]
+                  ? checkGuess(wordList[guess].word, answer)
+                  : null
+              }
+            />
           </div>
         ))}
       </div>
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 let letters = range(0, 5);
 
 function Guess({ guess }) {
   return (
     <p className="guess">
       {letters.map((letter) => (
-        <span key={letter} className="cell">
-          {guess && guess.word.split("")[letter]}
+        <span
+          key={letter}
+          className={"cell " + (guess && guess[letter].status)}
+        >
+          {guess && guess[letter].letter}
         </span>
       ))}
     </p>
